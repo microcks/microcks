a102614e9215499f48c4b23a4140c0d3180f8c3e
lbroudoux <laurent.broudoux@gmail.com>
2015-02-27 15:04:52 +0100

    Service relatives initial import

---
+++ src/main/java/com/github/lbroudoux/microcks/domain/Operation.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to Laurent Broudoux (the "Author") under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Author licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.lbroudoux.microcks.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * An Operation / action of a micro service. Holds information on
+ * messages constitution (inputName, outputName) and how dispatch
+ * request to them.
+ * @author laurent
+ */
+public class Operation {
+
+   private String name;
+   private String method;
+   private String inputName;
+   private String outputName;
+
+   private String dispatcher;
+   private String dispatcherRules;
+   private int defaultDelay;
+
+   private List<String> resourcePaths;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getMethod() {
+      return method;
+   }
+
+   public void setMethod(String method) {
+      this.method = method;
+   }
+
+   public String getInputName() {
+      return inputName;
+   }
+
+   public void setInputName(String inputName) {
+      this.inputName = inputName;
+   }
+
+   public String getOutputName() {
+      return outputName;
+   }
+
+   public void setOutputName(String outputName) {
+      this.outputName = outputName;
+   }
+
+   public String getDispatcher() {
+      return dispatcher;
+   }
+
+   public void setDispatcher(String dispatcher) {
+      this.dispatcher = dispatcher;
+   }
+
+   public String getDispatcherRules() {
+      return dispatcherRules;
+   }
+
+   public void setDispatcherRules(String dispatcherRules) {
+      this.dispatcherRules = dispatcherRules;
+   }
+
+   public int getDefaultDelay() {
+      return defaultDelay;
+   }
+
+   public void setDefaultDelay(int defaultDelay) {
+      this.defaultDelay = defaultDelay;
+   }
+
+   public List<String> getResourcePaths() {
+      return resourcePaths;
+   }
+
+   public void setResourcePaths(List<String> resourcePaths) {
+      this.resourcePaths = resourcePaths;
+   }
+
+   public void addResourcePath(String resourcePath) {
+      if (this.resourcePaths == null) {
+         this.resourcePaths = new ArrayList<>();
+      }
+      resourcePaths.add(resourcePath);
+   }
+}
