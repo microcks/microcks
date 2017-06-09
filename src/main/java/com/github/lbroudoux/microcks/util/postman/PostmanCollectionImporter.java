17190f4a2eb8f6fbe0eefdea73e575dc66549aee
lbroudoux <laurent.broudoux@gmail.com>
2017-06-09 17:52:57 +0200

    Adding support for Postman v1 collection format

--- src/main/java/com/github/lbroudoux/microcks/util/postman/PostmanCollectionImporter.java
+++ src/main/java/com/github/lbroudoux/microcks/util/postman/PostmanCollectionImporter.java
@@ -46,6 +46,9 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
    public static final String SERVICE_VERSION_PROPERTY = "version";
 
    private JsonNode collection;
+   // Flag telling if V2 format is used.
+   private boolean isV2Collection = false;
+
 
    /**
     * Build a new importer.
@@ -70,6 +73,42 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
 
       // Build a new service.
       Service service = new Service();
+
+      // Collection V2 as an info node.
+      if (collection.has("info")) {
+         isV2Collection = true;
+         fillServiceDefinitionV2(service);
+      } else {
+         fillServiceDefinitionV1(service);
+      }
+
+      // Then build its operations.
+      service.setOperations(extractOperations());
+
+      result.add(service);
+      return result;
+   }
+
+   private void fillServiceDefinitionV1(Service service) {
+      service.setName(collection.path("name").asText());
+      service.setType(ServiceType.REST);
+
+      String version = null;
+      String description = collection.path("description").asText();
+      if (description != null && description.indexOf(SERVICE_VERSION_PROPERTY + "=") != -1) {
+         description = description.substring(description.indexOf(SERVICE_VERSION_PROPERTY + "="));
+         description = description.substring(0, description.indexOf(' '));
+         if (description.split("=").length > 1) {
+            version = description.split("=")[1];
+         }
+      }
+      if (version == null){
+         // TODO Throw a typed exception here...
+      }
+      service.setVersion(version);
+   }
+
+   private void fillServiceDefinitionV2(Service service) {
       service.setName(collection.path("info").path("name").asText());
       service.setType(ServiceType.REST);
 
@@ -86,12 +125,6 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
          // TODO Throw a typed exception here...
       }
       service.setVersion(version);
-
-      // Then build its operations.
-      service.setOperations(extractOperations());
-
-      result.add(service);
-      return result;
    }
 
    @Override
@@ -103,6 +136,51 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
 
    @Override
    public Map<Request, Response> getMessageDefinitions(Service service, Operation operation) throws MockRepositoryImportException {
+
+      if (isV2Collection) {
+         return getMessageDefinitionsV2(service, operation);
+      } else {
+         return getMessageDefinitionsV1(service, operation);
+      }
+   }
+
+   private Map<Request, Response> getMessageDefinitionsV1(Service service, Operation operation) {
+      Map<Request, Response> result = new HashMap<Request, Response>();
+
+      Iterator<JsonNode> requests = collection.path("requests").elements();
+      while (requests.hasNext()) {
+         JsonNode requestNode = requests.next();
+         String method = requestNode.path("method").asText();
+         String url = requestNode.path("url").asText();
+         String resourcePath = extractResourcePath(url);
+
+         // Select item based onto operation Http verb (GET, POST, PUT, etc ...)
+         if (operation.getMethod().equals(method)) {
+            // ... then check is we have a matching resource path.
+            if (operation.getResourcePaths().contains(resourcePath)) {
+               JsonNode responseNode = requestNode.path("responses").get(0);
+
+               String dispatchCriteria = null;
+
+               if (DispatchStyles.URI_PARAMS.equals(operation.getDispatcher())) {
+                  dispatchCriteria = DispatchCriteriaHelper.extractFromURIParams(operation.getDispatcherRules(), url);
+               } else if (DispatchStyles.URI_PARTS.equals(operation.getDispatcher())) {
+                  dispatchCriteria = DispatchCriteriaHelper.extractFromURIPattern(operation.getName(), resourcePath);
+               } else if (DispatchStyles.URI_ELEMENTS.equals(operation.getDispatcher())) {
+                  dispatchCriteria = DispatchCriteriaHelper.extractFromURIPattern(operation.getName(), resourcePath);
+                  dispatchCriteria += DispatchCriteriaHelper.extractFromURIParams(operation.getDispatcherRules(), url);
+               }
+
+               Request request = buildRequest(requestNode, requestNode.path("name").asText());
+               Response response = buildResponse(responseNode, dispatchCriteria);
+               result.put(request, response);
+            }
+         }
+      }
+      return result;
+   }
+
+   private Map<Request, Response> getMessageDefinitionsV2(Service service, Operation operation) {
       Map<Request, Response> result = new HashMap<Request, Response>();
 
       Iterator<JsonNode> items = collection.path("item").elements();
@@ -142,15 +220,26 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
    private Request buildRequest(JsonNode requestNode, String name) {
       Request request = new Request();
       request.setName(name);
-      request.setHeaders(buildHeaders(requestNode.path("header")));
+
+      if (isV2Collection) {
+         request.setHeaders(buildHeaders(requestNode.path("header")));
+      } else {
+         request.setHeaders(buildHeaders(requestNode.path("headers")));
+      }
       return request;
    }
 
    private Response buildResponse(JsonNode responseNode, String dispatchCriteria) {
       Response response = new Response();
       response.setName(responseNode.path("name").asText());
-      response.setStatus(responseNode.path("code").asText());
-      response.setHeaders(buildHeaders(responseNode.path("header")));
+
+      if (isV2Collection) {
+         response.setStatus(responseNode.path("code").asText());
+         response.setHeaders(buildHeaders(responseNode.path("header")));
+      } else {
+         response.setStatus(responseNode.path("responseCode").path("code").asText());
+         response.setHeaders(buildHeaders(responseNode.path("headers")));
+      }
       if (response.getHeaders() != null) {
          for (Header header : response.getHeaders()) {
             if (header.getName().equalsIgnoreCase("Content-Type")) {
@@ -187,6 +276,59 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
     * Extract the list of operations from Collection.
     */
    private List<Operation> extractOperations() {
+
+      if (isV2Collection) {
+         return extractOperationsV2();
+      } else {
+         return extractOperationsV1();
+      }
+   }
+
+   private List<Operation> extractOperationsV1() {
+      // Items corresponding to same operations may be defined multiple times in Postman
+      // with different names and resource path. We have to track them to complete them in second step.
+      Map<String, Operation> collectedOperations = new HashMap<String, Operation>();
+
+      Iterator<JsonNode> requests = collection.path("requests").elements();
+      while (requests.hasNext()) {
+         JsonNode request = requests.next();
+         String requestName = request.path("name").asText();
+         // findByStatus (status=available) => findByStatus
+         String operationName = requestName.substring(0, requestName.indexOf(' '));
+
+         Operation operation = collectedOperations.get(operationName);
+         String url = request.path("url").asText();
+
+         if (operation == null) {
+            // Build a new operation.
+            operation = new Operation();
+            operation.setName(operationName);
+
+            // Complete with REST specific fields.
+            operation.setMethod(request.path("method").asText());
+
+            // Deal with dispatcher stuffs.
+            if (urlHasParameters(url)) {
+               operation.setDispatcherRules(DispatchCriteriaHelper.extractParamsFromURI(url));
+               operation.setDispatcher(DispatchStyles.URI_PARAMS);
+            }
+         }
+
+         // Extract resource path from complete URL found in collection.
+         String resourcePath = extractResourcePath(url);
+         operation.addResourcePath(resourcePath);
+         collectedOperations.put(operationName, operation);
+      }
+
+      // Check operations: if we got multiple resource paths for an Operation, we should
+      // extracts the variable parts of the path and update dispatch criteria accordingly.
+      // We can also update the operation name so that it reflects the resource path
+      sanitizeCollectedOperations(collectedOperations);
+
+      return new ArrayList<>(collectedOperations.values());
+   }
+
+   private List<Operation> extractOperationsV2() {
       // Items corresponding to same operations may be defined multiple times in Postman
       // with different names and resource path. We have to track them to complete them in second step.
       Map<String, Operation> collectedOperations = new HashMap<String, Operation>();
@@ -225,6 +367,12 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
       // Check operations: if we got multiple resource paths for an Operation, we should
       // extracts the variable parts of the path and update dispatch criteria accordingly.
       // We can also update the operation name so that it reflects the resource path
+      sanitizeCollectedOperations(collectedOperations);
+
+      return new ArrayList<>(collectedOperations.values());
+   }
+
+   private void sanitizeCollectedOperations(Map<String, Operation> collectedOperations) {
       for (Operation operation : collectedOperations.values()) {
          if (operation.getResourcePaths().size() > 1) {
             String partsCriteria = DispatchCriteriaHelper.extractPartsFromURIs(operation.getResourcePaths());
@@ -248,8 +396,6 @@ public class PostmanCollectionImporter implements MockRepositoryImporter {
             operation.setName(operation.getResourcePaths().get(0));
          }
       }
-
-      return new ArrayList<>(collectedOperations.values());
    }
 
    /**
--- src/test/java/com/github/lbroudoux/microcks/util/postman/PostmanCollectionImporterTest.java
+++ src/test/java/com/github/lbroudoux/microcks/util/postman/PostmanCollectionImporterTest.java
@@ -38,7 +38,102 @@ import static org.junit.Assert.fail;
 public class PostmanCollectionImporterTest {
 
    @Test
-   public void testSimpleProjectImport() {
+   public void testSimpleProjectImportV1() {
+      PostmanCollectionImporter importer = null;
+      try {
+         importer = new PostmanCollectionImporter("target/test-classes/com/github/lbroudoux/microcks/util/postman/PetstoreAPI-collection-v1.json");
+      } catch (IOException ioe) {
+         fail("Exception should not be thrown");
+      }
+      // Check that basic service properties are there.
+      List<Service> services = importer.getServiceDefinitions();
+      assertEquals(1, services.size());
+      Service service = services.get(0);
+      assertEquals("Petstore API", service.getName());
+      assertEquals(ServiceType.REST, service.getType());
+      assertEquals("1.0", service.getVersion());
+
+      // Check that resources have been parsed, correctly renamed, etc...
+      List<Resource> resources = importer.getResourceDefinitions(service);
+
+      // Check that operations and and input/output have been found.
+      assertEquals(2, service.getOperations().size());
+      for (Operation operation : service.getOperations()) {
+
+         if ("/v2/pet/findByStatus".equals(operation.getName())) {
+            // assertions for findByStatus.
+            assertEquals("GET", operation.getMethod());
+            assertEquals(1, operation.getResourcePaths().size());
+            assertEquals(DispatchStyles.URI_PARAMS, operation.getDispatcher());
+            assertEquals("/v2/pet/findByStatus", operation.getResourcePaths().get(0));
+
+            // Check that messages have been correctly found.
+            Map<Request, Response> messages = null;
+            try{
+               messages = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e){
+               e.printStackTrace();
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(1, messages.size());
+            Map.Entry<Request, Response> entry = messages.entrySet().iterator().next();
+            Request request = entry.getKey();
+            Response response = entry.getValue();
+            assertNotNull(request);
+            assertNotNull(response);
+            assertEquals("available response", response.getName());
+            assertEquals(9, response.getHeaders().size());
+            assertEquals("200", response.getStatus());
+            assertEquals("application/json", response.getMediaType());
+            assertEquals("?status=available?user_key=998bac0775b1d5f588e0a6ca7c11b852", response.getDispatchCriteria());
+            assertNotNull(response.getContent());
+         }
+         else if ("/v2/pet/{part1}".equals(operation.getName())) {
+            // assertions for findById.
+            assertEquals("GET", operation.getMethod());
+            assertEquals(2, operation.getResourcePaths().size());
+            assertEquals(DispatchStyles.URI_ELEMENTS, operation.getDispatcher());
+            assertTrue("/v2/pet/1".equals(operation.getResourcePaths().get(0))
+                  || "/v2/pet/2".equals(operation.getResourcePaths().get(0)));
+            assertTrue("/v2/pet/1".equals(operation.getResourcePaths().get(1))
+                  || "/v2/pet/2".equals(operation.getResourcePaths().get(1)));
+
+            // Check that messages have been correctly found.
+            Map<Request, Response> messages = null;
+            try {
+               messages = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               e.printStackTrace();
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(2, messages.size());
+            for (Map.Entry<Request, Response> entry : messages.entrySet()) {
+               Request request = entry.getKey();
+               Response response = entry.getValue();
+               assertNotNull(request);
+               assertNotNull(response);
+               if ("findById 2".equals(request.getName())) {
+                  assertEquals("2 response", response.getName());
+                  assertEquals(9, response.getHeaders().size());
+                  assertEquals("200", response.getStatus());
+                  assertEquals("application/json", response.getMediaType());
+                  assertEquals("/part1=2?user_key=998bac0775b1d5f588e0a6ca7c11b852", response.getDispatchCriteria());
+                  assertNotNull(response.getContent());
+               } else if ("findById 1 (404)".equals(request.getName())) {
+                  assertEquals("404", response.getStatus());
+                  assertEquals("/part1=1?user_key=998bac0775b1d5f588e0a6ca7c11b852", response.getDispatchCriteria());
+               } else {
+                  fail("Unknown request name: " + request.getName());
+               }
+            }
+         } else {
+            fail("Unknown operation name: " + operation.getName());
+         }
+      }
+   }
+
+   @Test
+   public void testSimpleProjectImportV2() {
       PostmanCollectionImporter importer = null;
       try {
          importer = new PostmanCollectionImporter("target/test-classes/com/github/lbroudoux/microcks/util/postman/PetstoreAPI-collection.json");
