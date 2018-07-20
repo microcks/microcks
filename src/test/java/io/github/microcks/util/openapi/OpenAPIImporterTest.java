d38a82b1ad136f51e5864f643c7244f1a6328d31
lbroudoux <laurent.broudoux@gmail.com>
2018-07-20 09:30:58 +0200

    Adding more tests on OpenAPI spec

--- src/test/java/io/github/microcks/util/openapi/OpenAPIImporterTest.java
+++ src/test/java/io/github/microcks/util/openapi/OpenAPIImporterTest.java
@@ -176,6 +176,145 @@ public class OpenAPIImporterTest {
       }
    }
 
+   @Test
+   public void testCompleteOpenAPIImportYAML() {
+      OpenAPIImporter importer = null;
+      try {
+         importer = new OpenAPIImporter("target/test-classes/io/github/microcks/util/openapi/cars-openapi-complete.yaml");
+      } catch (IOException ioe) {
+         fail("Exception should not be thrown");
+      }
+
+      // Check that basic service properties are there.
+      List<Service> services = null;
+      try {
+         services = importer.getServiceDefinitions();
+      } catch (MockRepositoryImportException e) {
+         fail("Exception should not be thrown");
+      }
+      assertEquals(1, services.size());
+      Service service = services.get(0);
+      assertEquals("OpenAPI Car API", service.getName());
+      Assert.assertEquals(ServiceType.REST, service.getType());
+      assertEquals("1.1.0", service.getVersion());
+
+      // Check that resources have been parsed, correctly renamed, etc...
+      List<Resource> resources = importer.getResourceDefinitions(service);
+      assertEquals(1, resources.size());
+      assertEquals(ResourceType.OPEN_API_SPEC, resources.get(0).getType());
+      assertTrue(resources.get(0).getName().startsWith(service.getName() + "-" + service.getVersion()));
+      assertNotNull(resources.get(0).getContent());
+
+      // Check that operations and input/output have been found.
+      assertEquals(4, service.getOperations().size());
+      for (Operation operation : service.getOperations()) {
+
+         if ("GET /owner/{owner}/car".equals(operation.getName())) {
+            assertEquals("GET", operation.getMethod());
+            assertEquals(DispatchStyles.URI_ELEMENTS, operation.getDispatcher());
+            assertEquals("owner ?? page && limit", operation.getDispatcherRules());
+
+            // Check that messages have been correctly found.
+            Map<Request, Response> messages = null;
+            try {
+               messages = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(1, messages.size());
+            assertEquals(1, operation.getResourcePaths().size());
+            assertEquals("/owner/laurent/car", operation.getResourcePaths().get(0));
+
+            for (Map.Entry<Request, Response> entry : messages.entrySet()) {
+               Request request = entry.getKey();
+               Response response = entry.getValue();
+               assertNotNull(request);
+               assertNotNull(response);
+               assertEquals("laurent_cars", request.getName());
+               assertEquals("laurent_cars", response.getName());
+               assertEquals("/owner=laurent?limit=20?page=0", response.getDispatchCriteria());
+               assertEquals("200", response.getStatus());
+               assertEquals("application/json", response.getMediaType());
+               assertNotNull(response.getContent());
+            }
+         }
+         else if ("POST /owner/{owner}/car".equals(operation.getName())) {
+            assertEquals("POST", operation.getMethod());
+            assertEquals(DispatchStyles.URI_PARTS, operation.getDispatcher());
+            assertEquals("owner", operation.getDispatcherRules());
+
+            // Check that messages have been correctly found.
+            Map<Request, Response> messages = null;
+            try {
+               messages = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(1, messages.size());
+            assertEquals(1, operation.getResourcePaths().size());
+            assertEquals("/owner/laurent/car", operation.getResourcePaths().get(0));
+
+            for (Map.Entry<Request, Response> entry : messages.entrySet()) {
+               Request request = entry.getKey();
+               Response response = entry.getValue();
+               assertNotNull(request);
+               assertNotNull(response);
+               assertEquals("laurent_307", request.getName());
+               assertEquals("laurent_307", response.getName());
+               assertEquals("/owner=laurent", response.getDispatchCriteria());
+               assertEquals("201", response.getStatus());
+               assertEquals("application/json", response.getMediaType());
+               assertNotNull(response.getContent());
+            }
+         }
+         else if ("GET /owner/{owner}/car/{car}/passenger".equals(operation.getName())) {
+            assertEquals("GET", operation.getMethod());
+            assertEquals(DispatchStyles.URI_PARTS, operation.getDispatcher());
+            assertEquals("owner && car", operation.getDispatcherRules());
+
+            // Check that messages have been correctly found.
+            Map<Request, Response> messages = null;
+            try {
+               messages = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(1, messages.size());
+            assertEquals(1, operation.getResourcePaths().size());
+            assertEquals("/owner/laurent/car/307/passenger", operation.getResourcePaths().get(0));
+
+            for (Map.Entry<Request, Response> entry : messages.entrySet()) {
+               Request request = entry.getKey();
+               Response response = entry.getValue();
+               assertNotNull(request);
+               assertNotNull(response);
+               assertEquals("laurent_307_passengers", request.getName());
+               assertEquals("laurent_307_passengers", response.getName());
+               assertEquals("/car=307/owner=laurent", response.getDispatchCriteria());
+               assertEquals("200", response.getStatus());
+               assertEquals("application/json", response.getMediaType());
+               assertNotNull(response.getContent());
+            }
+         }
+         else if ("POST /owner/{owner}/car/{car}/passenger".equals(operation.getName())) {
+            assertEquals("POST", operation.getMethod());
+            assertEquals(DispatchStyles.URI_PARTS, operation.getDispatcher());
+            assertEquals("owner && car", operation.getDispatcherRules());
+
+            // Check that messages have been correctly found.
+            Map<Request, Response> messages = null;
+            try {
+               messages = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(0, messages.size());
+         } else {
+            fail("Unknown operation name: " + operation.getName());
+         }
+      }
+   }
+
 
    private void importAndAssertOnSimpleOpenAPI(OpenAPIImporter importer) {
       // Check that basic service properties are there.
