93c8335f25e45978733383143a46e8dd05aaf267
lbroudoux <laurent.broudoux@gmail.com>
2019-03-04 18:01:49 +0100

    #123 Remove unused import

--- src/main/java/io/github/microcks/util/dispatcher/JsonExpressionEvaluator.java
+++ src/main/java/io/github/microcks/util/dispatcher/JsonExpressionEvaluator.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.util.regex.Pattern;
 
