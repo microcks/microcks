11ef74c2706e5af012a775b81a2b8ccd800babcf
Laurent Broudoux <laurent.broudoux@gmail.com>
2020-06-22 11:59:28 +0200

    #230 Fix more stuffs

--- install/docker-compose/setup-microcks-apt.sh
+++ install/docker-compose/setup-microcks-apt.sh
@@ -10,7 +10,7 @@ apt-get install -y jq docker-compose
 cd /opt
 echo "Retrieving microcks sources ..."
 git clone https://github.com/microcks/microcks.git
-if [ ${VERSION} != "latest" ]; then
+if [ "${VERSION}" != "latest" ]; then
   git checkout tags/${VERSION}
 fi
 
@@ -21,7 +21,7 @@ echo "Replacing hostname in configuration files ..."
 jq '.applications |= map(if .name == "microcks-app-js" then .redirectUris = [ "https://'${HOSTNAME}':8080/*" ] else . end)' keycloak-realm/microcks-realm-sample.json.bak > keycloak-realm/microcks-realm-sample.json
 
 perl -i.bak -pe 's|- KEYCLOAK_URL=https://docker.for.mac.localhost:8543/auth|#- KEYCLOAK_URL=https://docker.for.mac.localhost:8543/auth|' microcks.yml
-perl -i.bak -pe 's|#- KEYCLOAK_URL=https://localhost:8180/auth|- KEYCLOAK_URL=http://'${HOSTNAME}':8543/auth|' microcks.yml
+perl -i.bak -pe 's|#- KEYCLOAK_URL=https://localhost:8543/auth|- KEYCLOAK_URL=https://'${HOSTNAME}':8543/auth|' microcks.yml
 
 echo "Generating certificates for '$HOSTNAME' ..."
 mkdir keystore
@@ -30,7 +30,7 @@ mv ./keystore/server.crt ./keystore/tls.crt
 mv ./keystore/server.key ./keystore/tls.key
 mv ./keystore/server.p12 ./keystore/microcks.p12
 
-if [ ${VERSION} != "latest" ]; then
+if [ "${VERSION}" != "latest" ]; then
   echo "Replacing version in configuration files ..."
   perl -i.bak -pe 's|microcks/microcks:latest|microcks/microcks:${VERSION}|' microcks.yml
 fi
