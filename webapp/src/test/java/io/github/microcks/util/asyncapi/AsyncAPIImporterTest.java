e4b82b09ed0f4ba037bc2b22281e2d9ef6ae56cb
Laurent Broudoux <laurent.broudoux@gmail.com>
2021-04-14 16:51:40 +0200

    #387 Adding unit tests for AsyncAPI parametrized channels

--- webapp/src/test/java/io/github/microcks/util/asyncapi/AsyncAPIImporterTest.java
+++ webapp/src/test/java/io/github/microcks/util/asyncapi/AsyncAPIImporterTest.java
@@ -474,4 +474,160 @@ public class AsyncAPIImporterTest {
       }
    }
 
+   @Test
+   public void testAsyncAPIImportWithParametrizedChannel() {
+      AsyncAPIImporter importer = null;
+      try {
+         importer = new AsyncAPIImporter("target/test-classes/io/github/microcks/util/asyncapi/api-maintenance.async-api-spec.yaml", null);
+      } catch (IOException ioe) {
+         ioe.printStackTrace();
+         fail("Exception should not be thrown");
+      }
+
+      // Check that basic service properties are there.
+      List<Service> services = null;
+      try {
+         services = importer.getServiceDefinitions();
+      } catch (MockRepositoryImportException e) {
+         fail("Exception should not be thrown");
+      }
+      assertEquals(1, services.size());
+      Service service = services.get(0);
+      assertEquals("ApiEventService:maintenance", service.getName());
+      assertEquals(ServiceType.EVENT, service.getType());
+      assertEquals("0.0.3", service.getVersion());
+
+      // Check that resources have been parsed, correctly renamed, etc...
+      List<Resource> resources = importer.getResourceDefinitions(service);
+      assertEquals(1, resources.size());
+      assertEquals(ResourceType.ASYNC_API_SPEC, resources.get(0).getType());
+      assertTrue(resources.get(0).getName().startsWith(service.getName() + "-" + service.getVersion()));
+      assertNotNull(resources.get(0).getContent());
+
+      // Check that operations and input/output have been found.
+      assertEquals(1, service.getOperations().size());
+
+      for (Operation operation : service.getOperations()) {
+
+         if ("PUBLISH apim/elevator-co/api/V1/json/{resource_region_id}/{equipmentType}/{eventType}/{resourceType}/{resourceId}".equals(operation.getName())) {
+            assertEquals("PUBLISH", operation.getMethod());
+            assertEquals(DispatchStyles.URI_PARTS, operation.getDispatcher());
+            assertEquals("resource_region_id && equipmentType && eventType && resourceType && resourceId", operation.getDispatcherRules());
+
+            // Check that messages have been correctly found.
+            List<Exchange> exchanges = null;
+            try {
+               exchanges = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(2, exchanges.size());
+            assertEquals(2, operation.getResourcePaths().size());
+            assertEquals("apim/elevator-co/api/V1/json/fr/elevator/maintenance/elev-make-1/abc4711", operation.getResourcePaths().get(0));
+            assertEquals("apim/elevator-co/api/V1/json/de/elevator/maintenance/elev-make-2/xyz0815", operation.getResourcePaths().get(1));
+
+            for (Exchange exchange : exchanges) {
+               if (exchange instanceof UnidirectionalEvent) {
+                  UnidirectionalEvent event = (UnidirectionalEvent) exchange;
+                  EventMessage eventMessage = event.getEventMessage();
+
+                  if ("misalignment".equals(eventMessage.getName())) {
+                     assertEquals("/equipmentType=elevator/eventType=maintenance/resourceId=abc4711/resourceType=elev-make-1/resource_region_id=fr", eventMessage.getDispatchCriteria());
+                     assertEquals("application/json", eventMessage.getMediaType());
+                     assertNotNull(eventMessage.getContent());
+                  } else if ("doorfailure".equals(eventMessage.getName())) {
+                     assertEquals("/equipmentType=elevator/eventType=maintenance/resourceId=xyz0815/resourceType=elev-make-2/resource_region_id=de", eventMessage.getDispatchCriteria());
+                     assertEquals("application/json", eventMessage.getMediaType());
+                     assertNotNull(eventMessage.getContent());
+                  } else {
+                     fail("Event has the wrong name. Expecting misalignment or doorfailure");
+                  }
+               } else {
+                  fail("Exchange has the wrong type. Expecting UnidirectionalEvent");
+               }
+            }
+         } else {
+            fail("Unknown operation name: " + operation.getName());
+         }
+      }
+   }
+
+   @Test
+   public void testAsyncAPIImportWithParametrizedChannelGHMaster() {
+      AsyncAPIImporter importer = null;
+      try {
+         importer = new AsyncAPIImporter("target/test-classes/io/github/microcks/util/asyncapi/api-maintenance.async-api-spec-gh-master.yaml", null);
+      } catch (IOException ioe) {
+         ioe.printStackTrace();
+         fail("Exception should not be thrown");
+      }
+
+      // Check that basic service properties are there.
+      List<Service> services = null;
+      try {
+         services = importer.getServiceDefinitions();
+      } catch (MockRepositoryImportException e) {
+         fail("Exception should not be thrown");
+      }
+      assertEquals(1, services.size());
+      Service service = services.get(0);
+      assertEquals("ApiEventService:maintenance", service.getName());
+      assertEquals(ServiceType.EVENT, service.getType());
+      assertEquals("0.0.3", service.getVersion());
+
+      // Check that resources have been parsed, correctly renamed, etc...
+      List<Resource> resources = importer.getResourceDefinitions(service);
+      assertEquals(1, resources.size());
+      assertEquals(ResourceType.ASYNC_API_SPEC, resources.get(0).getType());
+      assertTrue(resources.get(0).getName().startsWith(service.getName() + "-" + service.getVersion()));
+      assertNotNull(resources.get(0).getContent());
+
+      // Check that operations and input/output have been found.
+      assertEquals(1, service.getOperations().size());
+
+      for (Operation operation : service.getOperations()) {
+
+         if ("PUBLISH apim/elevator-co/api/V1/json/{resource_region_id}/{equipmentType}/{eventType}/{resourceType}/{resourceId}".equals(operation.getName())) {
+            assertEquals("PUBLISH", operation.getMethod());
+            assertEquals(DispatchStyles.URI_PARTS, operation.getDispatcher());
+            assertEquals("resource_region_id && equipmentType && eventType && resourceType && resourceId", operation.getDispatcherRules());
+
+            // Check that messages have been correctly found.
+            List<Exchange> exchanges = null;
+            try {
+               exchanges = importer.getMessageDefinitions(service, operation);
+            } catch (Exception e) {
+               fail("No exception should be thrown when importing message definitions.");
+            }
+            assertEquals(2, exchanges.size());
+            assertEquals(2, operation.getResourcePaths().size());
+            assertEquals("apim/elevator-co/api/V1/json/fr/elevator/maintenance/elev-make-1/abc4711", operation.getResourcePaths().get(0));
+            assertEquals("apim/elevator-co/api/V1/json/de/elevator/maintenance/elev-make-2/xyz0815", operation.getResourcePaths().get(1));
+
+            for (Exchange exchange : exchanges) {
+               if (exchange instanceof UnidirectionalEvent) {
+                  UnidirectionalEvent event = (UnidirectionalEvent) exchange;
+                  EventMessage eventMessage = event.getEventMessage();
+
+                  if ("apim/elevator-co/api/V1/json/{resource_region_id}/{equipmentType}/{eventType}/{resourceType}/{resourceId}-0".equals(eventMessage.getName())) {
+                     assertEquals("/equipmentType=elevator/eventType=maintenance/resourceId=abc4711/resourceType=elev-make-1/resource_region_id=fr", eventMessage.getDispatchCriteria());
+                     assertEquals("application/json", eventMessage.getMediaType());
+                     assertNotNull(eventMessage.getContent());
+                  } else if ("apim/elevator-co/api/V1/json/{resource_region_id}/{equipmentType}/{eventType}/{resourceType}/{resourceId}-1".equals(eventMessage.getName())) {
+                     assertEquals("/equipmentType=elevator/eventType=maintenance/resourceId=xyz0815/resourceType=elev-make-2/resource_region_id=de", eventMessage.getDispatchCriteria());
+                     assertEquals("application/json", eventMessage.getMediaType());
+                     assertNotNull(eventMessage.getContent());
+                  } else {
+                     fail("Event has the wrong name. Expecting apim/elevator-co/api/V1/json/{resource_region_id}/{equipmentType}/{eventType}/{resourceType}/{resourceId}-0" +
+                           " or apim/elevator-co/api/V1/json/{resource_region_id}/{equipmentType}/{eventType}/{resourceType}/{resourceId}-1");
+                  }
+               } else {
+                  fail("Exchange has the wrong type. Expecting UnidirectionalEvent");
+               }
+            }
+         } else {
+            fail("Unknown operation name: " + operation.getName());
+         }
+      }
+   }
 }
