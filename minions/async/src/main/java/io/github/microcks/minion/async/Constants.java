1ecbe59f4e568393602978eec578a38a54e9903b
Laurent Broudoux <laurent.broudoux@gmail.com>
2021-11-25 10:26:38 +0100

    #513 Adding application/avro as valid Avro content-type

--- minions/async/src/main/java/io/github/microcks/minion/async/Constants.java
+++ minions/async/src/main/java/io/github/microcks/minion/async/Constants.java
@@ -31,5 +31,5 @@ public class Constants {
    public static final String REGISTRY_AVRO_ENCODING = "REGISTRY";
 
    /** Supported content-types representing Avro binary encoded content. */
-   public static final List<String> AVRO_BINARY_CONTENT_TYPES = Arrays.asList("avro/binary", "application/octet-stream");
+   public static final List<String> AVRO_BINARY_CONTENT_TYPES = Arrays.asList("avro/binary", "application/octet-stream", "application/avro");
 }
