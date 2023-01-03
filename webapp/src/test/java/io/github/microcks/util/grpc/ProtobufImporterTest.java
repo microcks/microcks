4ded1bc8460b99e7d665156cd8093c4e318c838b
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-01-03 10:39:23 +0100

    chore #729: adding example test of using option

--- webapp/src/test/java/io/github/microcks/util/grpc/ProtobufImporterTest.java
+++ webapp/src/test/java/io/github/microcks/util/grpc/ProtobufImporterTest.java
@@ -96,6 +96,72 @@ public class ProtobufImporterTest {
    }
 
    @Test
+   public void testProtobufWithOptionsImport() {
+      ProtobufImporter importer = null;
+      try {
+         importer = new ProtobufImporter("target/test-classes/io/github/microcks/util/grpc/hello-v1-option.proto", null);
+      } catch (IOException ioe) {
+         fail("Exception should not be thrown");
+      }
+
+      // Check that basic service properties are there.
+      List<Service> services = null;
+      try {
+         services = importer.getServiceDefinitions();
+      } catch (MockRepositoryImportException e) {
+         fail("Service definition import should not fail");
+      }
+      assertEquals(1, services.size());
+
+      Service service = services.get(0);
+      assertEquals("HelloService", service.getName());
+      assertEquals(ServiceType.GRPC, service.getType());
+      assertEquals("v1", service.getVersion());
+      assertEquals("io.github.microcks.grpc.hello.v1", service.getXmlNS());
+
+      // Check that resources have been parsed, correctly renamed, etc...
+      List<Resource> resources = null;
+      try {
+         resources = importer.getResourceDefinitions(service);
+      } catch (MockRepositoryImportException mrie) {
+         fail("Resource definition import should not fail");
+      }
+      assertEquals(2, resources.size());
+      for (Resource resource : resources) {
+         assertNotNull(resource.getContent());
+         if (ResourceType.PROTOBUF_SCHEMA.equals(resource.getType())) {
+            assertEquals("HelloService-v1.proto", resource.getName());
+         } else if (ResourceType.PROTOBUF_DESCRIPTOR.equals(resource.getType())) {
+            assertEquals("HelloService-v1.pbb", resource.getName());
+
+            try {
+               // Check Protobuf Descriptor.
+               byte[] decodedBinaryPB = Base64.getDecoder().decode(resource.getContent().getBytes("UTF-8"));
+
+               DescriptorProtos.FileDescriptorSet fds = DescriptorProtos.FileDescriptorSet.parseFrom(decodedBinaryPB);
+               assertEquals(4, fds.getFileCount());
+               assertEquals("google/api/http.proto", fds.getFile(0).getName());
+               assertEquals("google/api/annotations.proto", fds.getFile(2).getName());
+               assertEquals("google/protobuf/descriptor.proto", fds.getFile(1).getName());
+               assertEquals("hello-v1-option.proto", fds.getFile(3).getName());
+            } catch (Exception e) {
+               fail("Protobuf file descriptor is not correct");
+            }
+         } else {
+            fail("Resource has not the expected type");
+         }
+      }
+
+      // Check that operations and input/output have been found.
+      assertEquals(1, service.getOperations().size());
+
+      Operation operation = service.getOperations().get(0);
+      assertEquals("greeting", operation.getName());
+      assertEquals(".io.github.microcks.grpc.hello.v1.HelloRequest", operation.getInputName());
+      assertEquals(".io.github.microcks.grpc.hello.v1.HelloResponse", operation.getOutputName());
+   }
+
+   @Test
    public void testProtobufWithDependenciesImport() {
       ProtobufImporter importer = null;
       try {
