8c61ef78bebf3c8479813ed6c57802425d9260e8
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-01-19 11:54:59 +0100

    fix #737: publish headers for AMQP messages

--- minions/async/src/main/java/io/github/microcks/minion/async/producer/AMQPProducerManager.java
+++ minions/async/src/main/java/io/github/microcks/minion/async/producer/AMQPProducerManager.java
@@ -180,6 +180,7 @@ public class AMQPProducerManager {
                renderedHeaders.add(renderedHeader);
             }
          }
+         return renderedHeaders;
       }
       return null;
    }
