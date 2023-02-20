edfb6b3d14a19e5670cd0afe46f8a7d436635377
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-02-20 11:07:41 +0100

    fix #739: Remove import/create for user only. Make it coherent throughout single and multi-tenant modes.

--- webapp/src/main/webapp/src/app/pages/importers/importers.page.ts
+++ webapp/src/main/webapp/src/app/pages/importers/importers.page.ts
@@ -363,8 +363,8 @@ export class ImportersPageComponent implements OnInit {
       }
       return false;
     }
-    // Default is true to keep behaviour of previous releases.
-    return true;
+    // Default is manager to keep coherent behaviour with multi-tenant feature.
+    return this.hasRole('manager');
   }
 
   public hasRole(role: string): boolean {
