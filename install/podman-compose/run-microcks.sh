81200a6d1658d4a1cc4448bfed7570778ec297d5
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-06-13 21:34:45 +0200

    fix: #568 rationalize templates and change script to adapt to OS

--- install/podman-compose/run-microcks.sh
+++ install/podman-compose/run-microcks.sh
@@ -1,39 +1,71 @@
 #!/bin/bash
 
+#
+# Licensed to Laurent Broudoux (the "Author") under one or more contributor license agreements.
+# See the NOTICE file distributed with this work for additional information regarding copyright
+# ownership. Author licenses this file to you under the Apache License, Version 2.0 (the  "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+#
+
 mkdir -p microcks-data || exit 1
 
-# The chosen podman-compose template depends on who is running this script
-if [ "$UID" -eq 0 ]; then
-  # We are root !
-  template="microcks-template.yml"
-  cmd_prefix="sudo"
-else
-  echo "Running rootless containers..."
-  template="microcks-template-rootless.yml"
-  cmd_prefix=""
-fi
+# Machine name on macos.
+machine_name="podman-machine-default"
 
-# Find the host ip address on linux systems
-host_ip="$(ip -o route get to 8.8.8.8 2>/dev/null | sed -n 's/.*src \([0-9.]\+\).*/\1/p')"
-if [ -z "$host_ip" ]; then
-  # Fallback method
-  iface="$(awk -F "\t" '$2 == "00000000" { print $1 }' /proc/net/route 2>/dev/null)"
-  host_ip="$(ifconfig "$iface" 2>/dev/null |awk '$1 == "inet" { print $2 }')"
+# Assuming default template, no addon and no command prefix.
+template="";
+addon="";
+cmd_prefix="";
+
+# Parse argument to adjust template and addon.
+if [ -n "$1" ]; then
+  if [ "$1" == "dev" ]; then
+    template="-devmode"
+  elif [ "$1" == "async" ]; then
+    addon="-f microcks-template-async-addon.yml"
+  fi
 fi
 
 # Generate a podman-compose file from the supplied template
-echo "Discovered host IP address: ${host_ip:-none}" 
-sed "s/__HOST__/$host_ip/" "$template" > microcks.yml || exit 1
+cp ./microcks-template"$template".yml ./microcks.yml || exit 1;
+
+if [ "$(uname)" == "Darwin" ]; then
+  # Change permission on microcks-data
+  chmod 777 ./microcks-data
+
+  echo
+  echo "On macos, need to get the userid and groupid from postman machine."
+  echo "Assuming this machine is named 'podman-machine-default'. Change name in script otherwise."
+  idStr="$(podman machine ssh $machine_name id)" || exit 1
+
+  # The above command should give something like:
+  #idStr="uid=501(core) gid=1000(core) groups=1000(core),4(adm),10(wheel),16(sudo),190(systemd-journal) context=unconfined_u:unconfined_r:unconfined_t:s0-s0:c0.c1023"
+
+  # Parse uid, gid and sed in enable user directive on mongo container.
+  machine_uid=$(echo $idStr | awk '{split($1,element,"="); split(element[2],part,"("); print part[1]}')
+  machine_gid=$(echo $idStr | awk '{split($2,element,"="); split(element[2],part,"("); print part[1]}')
+  sed -i '' 's=#user: \"501:1000\"=user: \"'"$machine_uid"':'"$machine_gid"'\"=g' ./microcks.yml || exit 1
+
+  cmd_prefix="--podman-run-args='--userns=keep-id:uid=$machine_uid,gid=$machine_gid'"
+fi
 
 echo
 echo "Starting Microcks using podman-compose ..."
 echo "------------------------------------------"
-echo "Stop it with: $cmd_prefix podman-compose -f microcks.yml stop"
-echo "Re-launch it with: $cmd_prefix podman-compose -f microcks.yml start"
-echo "Clean everything with: $cmd_prefix podman-compose -f microcks.yml down"
+echo "Stop it with: podman-compose -f microcks.yml $addon $cmd_prefix stop"
+echo "Re-launch it with: podman-compose -f microcks.yml $addon $cmd_prefix start"
+echo "Clean everything with: podman-compose -f microcks.yml $addon $cmd_prefix down"
 echo "------------------------------------------"
 echo "Go to https://localhost:8080 - first login with admin/microcks123"
 echo "Having issues? Check you have changed microcks.yml to your platform"
 echo
 
-podman-compose -f microcks.yml up -d
+podman-compose -f microcks.yml $addon $cmd_prefix up -d
