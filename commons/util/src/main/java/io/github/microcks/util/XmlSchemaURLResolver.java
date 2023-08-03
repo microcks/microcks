7d5ebae30fed0aa9f11895ec42709e2555a261f0
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-08-03 12:29:14 +0200

    feat: #884 First shoot of independant utilities for Xml/XSD/Wsdl/Soap manipulation Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

---
+++ commons/util/src/main/java/io/github/microcks/util/XmlSchemaURLResolver.java
@@ -0,0 +1,181 @@
+/*
+ * Licensed to Laurent Broudoux (the "Author") under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Author licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.github.microcks.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * An implementation of LSResourceResolver that may use local cache for standard schemas (xml.xsd)
+ * of an URL resolver for relative references to XSD that may be embedded into other XSD.
+ * @author laurent
+ */
+public class XmlSchemaURLResolver implements LSResourceResolver {
+
+   /** A commons logger for diagnostic messages. */
+   private static Logger log = LoggerFactory.getLogger(XmlSchemaURLResolver.class);
+
+   private static final Map<String, String> LOCAL_RESOLUTIONS = Map.of(
+         "http://www.w3.org/2001/xml.xsd", "xml.xsd"
+   );
+
+   private String baseResourceURL;
+
+   /**
+    * Build a XmlSchemaURLResolver with base URL for resolving relative dependencies.
+    * @param baseResourceURL The base URL to consider for relative dependencies
+    */
+   public XmlSchemaURLResolver(String baseResourceURL) {
+      this.baseResourceURL = baseResourceURL;
+   }
+
+   @Override
+   public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
+      log.debug("Resolving resource with systemId: {}", systemId);
+
+      InputStream resourceStream = null;
+      if (LOCAL_RESOLUTIONS.containsKey(systemId)) {
+         log.debug("Got a local resolution, loading it from classloader");
+         resourceStream = this.getClass().getClassLoader().getResourceAsStream(LOCAL_RESOLUTIONS.get(systemId));
+      } else if (baseResourceURL != null && (!systemId.startsWith("http://") || !systemId.startsWith("https://"))) {
+         log.debug("Got a baseResourceURL defined and relative reference, loading it from URL");
+         String sanitizedSystemId = systemId;
+         if (systemId.startsWith("./")) {
+            sanitizedSystemId = systemId.substring(2);
+         }
+         try {
+            URL resourceURL = new URL(baseResourceURL + (baseResourceURL.endsWith("/") ? "":"/") + sanitizedSystemId);
+            resourceStream = resourceURL.openStream();
+         } catch (Exception e) {
+            log.error("Failed to open stream on {}/{}", baseResourceURL, sanitizedSystemId, e);
+         }
+      }
+
+      if (resourceStream != null) {
+         Input input = new Input();
+         input.setSystemId(systemId);
+         input.setPublicId(publicId);
+         input.setBaseURI(baseURI);
+         input.setCharacterStream(new InputStreamReader(resourceStream));
+         return input;
+      }
+      // Let default behaviour happen!
+      return null;
+   }
+
+   public class Input implements LSInput {
+
+      private Reader characterStream;
+      private InputStream byteStream;
+      private String stringData;
+      private String systemId;
+      private String publicId;
+      private String baseURI;
+      private String encoding;
+      private boolean certifiedText;
+
+      @Override
+      public Reader getCharacterStream() {
+         return characterStream;
+      }
+
+      @Override
+      public void setCharacterStream(Reader characterStream) {
+         this.characterStream = characterStream;
+      }
+
+      @Override
+      public InputStream getByteStream() {
+         return byteStream;
+      }
+
+      @Override
+      public void setByteStream(InputStream byteStream) {
+         this.byteStream = byteStream;
+      }
+
+      @Override
+      public String getStringData() {
+         return stringData;
+      }
+
+      @Override
+      public void setStringData(String stringData) {
+         this.stringData = stringData;
+      }
+
+      @Override
+      public String getSystemId() {
+         return systemId;
+      }
+
+      @Override
+      public void setSystemId(String systemId) {
+         this.systemId = systemId;
+      }
+
+      @Override
+      public String getPublicId() {
+         return publicId;
+      }
+
+      @Override
+      public void setPublicId(String publicId) {
+         this.publicId = publicId;
+      }
+
+      @Override
+      public String getBaseURI() {
+         return baseURI;
+      }
+
+      @Override
+      public void setBaseURI(String baseURI) {
+         this.baseURI = baseURI;
+      }
+
+      @Override
+      public String getEncoding() {
+         return encoding;
+      }
+
+      @Override
+      public void setEncoding(String encoding) {
+         this.encoding = encoding;
+      }
+
+      @Override
+      public boolean getCertifiedText() {
+         return false;
+      }
+
+      @Override
+      public void setCertifiedText(boolean certifiedText) {
+         this.certifiedText = certifiedText;
+      }
+   }
+}
---
+++ commons/util/src/main/java/io/github/microcks/util/soap/SoapMessageValidator.java
@@ -0,0 +1,187 @@
+/*
+ * Licensed to Laurent Broudoux (the "Author") under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Author licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.github.microcks.util.soap;
+
+import io.github.microcks.util.XmlSchemaValidator;
+import io.github.microcks.util.XmlUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Helper class for validating Soap messages objects against their WSDL/XSD schemas. Supported versions
+ * of Soap are Soap 1.1 and Soap 1.2.
+ * @author laurent
+ */
+public class SoapMessageValidator {
+
+
+   /** A commons logger for diagnostic messages. */
+   private static Logger log = LoggerFactory.getLogger(SoapMessageValidator.class);
+
+   private static final String SOAP_ENVELOPE_SCHEMA = "soap-envelope.xsd";
+
+   private static final String SOAP_ENVELOPE_12_SCHEMA = "soap-envelope-12.xsd";
+
+   /** Soap 1.1 envelope public namespace. */
+   public static final String SOAP_ENVELOPE_NS = "http://schemas.xmlsoap.org/soap/envelope/";
+   /** Soap 1.2 envelope public namespace. */
+   public static final String SOAP_ENVELOPE_12_NS = "http://www.w3.org/2003/05/soap-envelope";
+
+   private static final Pattern XML_NS_CAPTURE_PATTERN = Pattern.compile("xmlns:(\\w+)=\"([^\"]*)\"", Pattern.DOTALL);
+   private static final Pattern START_BODY_PATTERN = Pattern.compile("(.*):Body>");
+   private static final Pattern END_BODY_PATTERN = Pattern.compile("</(.*):Body>");
+
+
+   /**
+    * Validate that given message has a well-formed Soap Envelope. This could Soap 1.1 or 1.2 envelope.
+    * @param message The Soap message to validate.
+    * @return A list of validation error that's empty if everything's ok.
+    */
+   public static List<String> validateSoapEnvelope(String message) {
+      List<String> errors = new ArrayList<>();
+
+      InputStream schemaStream;
+      if (message.contains(SOAP_ENVELOPE_12_NS)) {
+         schemaStream = SoapMessageValidator.class.getClassLoader().getResourceAsStream(SOAP_ENVELOPE_12_SCHEMA);
+      } else if (message.contains(SOAP_ENVELOPE_NS)) {
+         schemaStream = SoapMessageValidator.class.getClassLoader().getResourceAsStream(SOAP_ENVELOPE_SCHEMA);
+      } else {
+         errors.add("Soap envelope does not appear to be valid: unrecognized namespace");
+         return errors;
+      }
+
+      try {
+         errors.addAll(XmlSchemaValidator.validateXml(schemaStream, message));
+      } catch (Exception e) {
+         log.error("Exception while validating Soap envelope for message: {}", e.getMessage());
+         errors.add("Exception while validating Soap envelope for message: " + e.getMessage());
+      }
+
+      log.debug("SoapEnvelope validation errors: " + errors.size());
+      return errors;
+   }
+
+   /**
+    * Validate a complete Soap message: Soap envelope + payload included into Soap body against a WSDL content.
+    * @param wsdlContent The reference WSDL as a String
+    * @param partQName The qualified name of the element that is expected into Soap body.
+    * @param message The Soap message as a String
+    * @param resourceUrl AN optional resource url where includes in WSDL may be resolved
+    * @return A list of validation error that's empty if everything's ok.
+    */
+   public static List<String> validateSoapMessage(String wsdlContent, QName partQName, String message, String resourceUrl) {
+      List<String> errors = new ArrayList<>();
+
+      // Start validating envelope.
+      errors.addAll(validateSoapEnvelope( message));
+
+      try {
+         // First thing is to parse WSDL to extract types schema.
+         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+         factory.setNamespaceAware(true);
+         DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+
+         Element wsdlElement = documentBuilder.parse(new InputSource(new StringReader(wsdlContent))).getDocumentElement();
+         Element wsdlTypes = XmlUtil.getUniqueDirectChild(wsdlElement, XmlUtil.WSDL_NS, "types");
+         Element xsSchema = XmlUtil.getUniqueDirectChild(wsdlTypes, XmlUtil.XML_SCHEMA_NS, "schema");
+
+         // Schema may not contain all the needed xmlns declaration found at the definitions level.
+         // We have to report it before extracting a standalone schema.
+         NamedNodeMap definitionsAttributes = wsdlElement.getAttributes();
+         for (int i=0; i<definitionsAttributes.getLength(); i++) {
+            Node attribute = definitionsAttributes.item(i);
+            String name = attribute.getNodeName();
+            if (name.startsWith("xmlns:") && !xsSchema.hasAttribute(name)) {
+               xsSchema.setAttribute(name, attribute.getNodeValue());
+            }
+         }
+
+         TransformerFactory transformerFactory = TransformerFactory.newInstance();
+         Transformer transformer = transformerFactory.newTransformer();
+         transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+         transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+
+         Writer out = new StringWriter();
+         transformer.transform(new DOMSource(xsSchema), new StreamResult(out));
+
+         String schemaContent = out.toString();
+
+         // Before extracting and validating body, we should capture all namespaces declaration to keep record
+         // for later reinject them into body.
+         Matcher nsMatcher = XML_NS_CAPTURE_PATTERN.matcher(message);
+         StringBuilder nsBuilder = new StringBuilder();
+         while (nsMatcher.find()) {
+            nsBuilder.append(" ").append(nsMatcher.group(0));
+         }
+
+         // Then we have to extract body payload from soap envelope. We'll use regexp for that.
+         String body = message;
+         Matcher sbMatcher = START_BODY_PATTERN.matcher(body);
+         if (sbMatcher.find()) {
+            body = body.substring(sbMatcher.end());
+            Matcher ebMatcher = END_BODY_PATTERN.matcher(body);
+            if (ebMatcher.find()) {
+               body = body.substring(0, ebMatcher.start());
+            }
+         }
+
+         // Remove all namespaces declaration from body to avoid duplicates.
+         body = body.replaceAll("\s" + XML_NS_CAPTURE_PATTERN.pattern(), "");
+
+         // Then we have to reintegrate namespace declarations in englobing element.
+         body = body.substring(0, body.indexOf('>')) + nsBuilder + body.substring(body.indexOf('>'));
+
+         log.debug("Soap message body to validate: {}", body);
+
+         // And finally validate everything using that body ;-)
+         errors.addAll(XmlSchemaValidator.validateXml(
+                     new ByteArrayInputStream(schemaContent.getBytes(StandardCharsets.UTF_8)), body, resourceUrl));
+      } catch (Exception e) {
+         log.error("Exception while validating Soap message: {}", e.getMessage());
+         errors.add("Exception while validating Soap message: " + e.getMessage());
+      }
+
+      log.debug("SoapMessage validation errors: " + errors.size());
+      return errors;
+   }
+}
