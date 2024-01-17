6f44a29b8991799b12827bb749d3b98f8811f9fe
Laurent Broudoux <laurent.broudoux@gmail.com>
2024-01-17 11:30:57 +0100

    feat: #992 Excluding Otel agent from app uber jar and fixing code style Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- webapp/src/main/java/io/github/microcks/config/MicrometerConfiguration.java
+++ webapp/src/main/java/io/github/microcks/config/MicrometerConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.microcks.config;
 
 import java.util.Map;
@@ -10,31 +25,34 @@ import org.springframework.web.servlet.HandlerMapping;
 import io.micrometer.common.KeyValue;
 import io.micrometer.common.KeyValues;
 
+/**
+ * Allows the configuration of the Micrometer observation, adding extra tags for
+ * tracking service and version in metrics.
+ */
 @Component
 public class MicrometerConfiguration extends DefaultServerRequestObservationConvention {
 
-    @Override
-    public KeyValues getLowCardinalityKeyValues(ServerRequestObservationContext context) {
-        // here, we just want to have an additional KeyValue to the observation, keeping
-        // the default values
-        return super.getLowCardinalityKeyValues(context).and(additionalTags(context));
-    }
+   @Override
+   public KeyValues getLowCardinalityKeyValues(ServerRequestObservationContext context) {
+      // Here, we just want to have an additional KeyValue to the observation, keeping the default values.
+      return super.getLowCardinalityKeyValues(context).and(additionalTags(context));
+   }
 
-    protected KeyValues additionalTags(ServerRequestObservationContext context) {
-        KeyValues keyValues = KeyValues.empty();
+   protected KeyValues additionalTags(ServerRequestObservationContext context) {
+      KeyValues keyValues = KeyValues.empty();
 
-        Map<String, String> pathVariables = (Map<String, String>) context.getCarrier()
-                .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-        if (pathVariables != null) {
-            String service = pathVariables.get("service");
-            String version = pathVariables.get("version");
-            if (service != null) {
-                keyValues = keyValues.and(KeyValue.of("service", service));
-            }
-            if (version != null) {
-                keyValues = keyValues.and(KeyValue.of("version", version));
-            }
-        }
-        return keyValues;
-    }
+      Map<String, String> pathVariables = (Map<String, String>) context.getCarrier()
+            .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+      if (pathVariables != null) {
+         String service = pathVariables.get("service");
+         String version = pathVariables.get("version");
+         if (service != null) {
+            keyValues = keyValues.and(KeyValue.of("service", service));
+         }
+         if (version != null) {
+            keyValues = keyValues.and(KeyValue.of("version", version));
+         }
+      }
+      return keyValues;
+   }
 }
