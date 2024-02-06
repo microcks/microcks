c9d946f0f2fc5e9cab50ce53f5e9230ccfeae22b
Appadoo Apoorva Srinivas <apoorva.appadoo@orange.com>
2024-02-06 15:59:49 +0100

    fix: some tests
    Signed-off-by: Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>

--- commons/model/src/main/java/io/github/microcks/util/Sanitizer.java
+++ commons/model/src/main/java/io/github/microcks/util/Sanitizer.java
@@ -4,21 +4,18 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Sanitizer {
-  private static final String ALLOWED_CHARS = "0123456789abcdefghIjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$-_.+!*'(),";
-  private static Set<Character> allowedCharSet;
+  private static final String ALLOWED_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$-_.+!*'(),";
+  private static Set<Character> ALLOWED_CHARS_SET = ALLOWED_CHARS
+      .chars()
+      .mapToObj(c -> (char) c)
+      .collect(Collectors.toSet());
   private static final Character REPLACE_CHAR = '-';
-
-  {
-    allowedCharSet = ALLOWED_CHARS.chars()
-        .mapToObj(c -> (char) c)
-        .collect(Collectors.toSet());
-  }
 
   public static String urlSanitize(String name) {
     StringBuilder sanitized = new StringBuilder();
 
     for (char c : name.toCharArray()) {
-      sanitized.append(allowedCharSet.contains(c) ? c : REPLACE_CHAR);
+      sanitized.append(ALLOWED_CHARS_SET.contains(c) ? c : REPLACE_CHAR);
     }
 
     return sanitized.toString();
