e32f58ed63e874382b52154bb562ae4b74920dbd
Appadoo Apoorva Srinivas <apoorva.appadoo@orange.com>
2024-02-12 15:20:04 +0100

    fix: resolve Exemple Node
    Signed-off-by: Appadoo Apoorva Srinivas <apoorva.appadoo@orange.com>

--- webapp/src/main/java/io/github/microcks/util/openapi/OpenAPIImporter.java
+++ webapp/src/main/java/io/github/microcks/util/openapi/OpenAPIImporter.java
@@ -205,7 +205,7 @@ public class OpenAPIImporter extends AbstractJsonRepositoryImporter implements M
                         Iterator<String> exampleNames = examplesNode.fieldNames();
                         while (exampleNames.hasNext()) {
                            String exampleName = exampleNames.next();
-                           JsonNode example = examplesNode.path(exampleName);
+                           JsonNode example = followRefIfAny(examplesNode.path(exampleName));
 
                            // We should have everything at hand to build response here.
                            Response response = new Response();
