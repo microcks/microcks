aaa4e4ef0e4698801675eb4e281a504a336341ee
Laurent Broudoux <laurent.broudoux@gmail.com>
2024-03-06 16:58:49 +0100

    chore: #870 Make the benchmark more representative and configurable Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- benchmark/README.md
+++ benchmark/README.md
@@ -1,17 +1,28 @@
 # K6 loadtesting on Microcks mock services
 
 1. Start you Microck instance 
-2. Import OpenBanking.org.uk APIs / Read/Write API - Account and Transaction.
-3. Import MicrocksIO Samples APIs / petstore-api-postman v. 1.0
+2. Import MicrocksIO Samples APIs / Pastry API - 2.0
+3. Import MicrocksIO Samples APIs / Movie Graph API
+4. Import MicrocksIO Samples APIs / HelloService Mock
+5. Import any other mock if you want to have more APIs to browse
 
 ## env vars
+
 ```
 export MICROCKS_BASE_URL=http://172.31.243.54:8080
 export PROMETHEUS_RW_URL=http://172.31.243.54:9080/api/v1/write
 export K6_VERSION=0.48.0
 ```
 
+If you're running Microcks locally (via docker-compose or other), you may use thes ones:
+
+```
+export MICROCKS_BASE_URL=http://host.docker.internal:8080
+export K6_VERSION=0.48.0
+```
+
 ## Simple script execution
+
 ```
 docker run --rm -i \
     -e BASE_URL=${MICROCKS_BASE_URL} \
--- benchmark/bench-microcks.js
+++ benchmark/bench-microcks.js
@@ -6,59 +6,206 @@ const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080';
 
 // Define the options for your test
 export let options = {
+    /*
     stages: [
         { duration: '1m', target: 50 }, // Ramp up to 50 virtual users over 1 minute
         { duration: '3m', target: 50 }, // Stay at 50 virtual users for 3 minutes
-        { duration: '30s', target: 0 }   // Ramp down to 0 virtual users over 1 minute
+        { duration: '30s', target: 0 }   // Ramp down to 0 virtual users over 30 seconds
     ],
+    */
+    scenarios: {
+      browse: {
+        executor: 'constant-vus',
+        exec: 'browse',
+        vus: 20,
+        duration: '1m',
+      },
+      invokeRESTMocks: {
+        executor: 'per-vu-iterations',
+        exec: 'invokeRESTMocks',
+        vus: 40,
+        iterations: 200,
+        startTime: '5s',
+        maxDuration: '2m',
+      },
+      invokeGraphQLMocks: {
+        executor: 'per-vu-iterations',
+        exec: 'invokeGraphQLMocks',
+        vus: 20,
+        iterations: 100,
+        startTime: '10s',
+        maxDuration: '2m',
+      },
+      invokeSOAPMocks: {
+        executor: 'per-vu-iterations',
+        exec: 'invokeSOAPMocks',
+        vus: 5,
+        iterations: 5,
+        startTime: '15s',
+        maxDuration: '2m',
+      }
+    }
 };
 
-// Define the main function that represents your test scenario
-export default function () {
-
-    /* ###################### */
-    /* ### Step 01        ### */
-    /* ###################### */
-    let step01 = http.get(`${BASE_URL}/rest/Openbanking.co.uk+Account+and+Transaction+API+v3.1.0/3.1.0/open-banking/v3.1/aisp/account-access-consents/urn-alphabank-intent-88380`);
-    check(step01, {
-        'Step 01 Status is 200': (r) => r.status === 200,
+/* Simulate users browsing the API repository and getting details. */
+export function browse() {
+    const servicesRes = http.get(`${BASE_URL}/api/services`);
+    check(servicesRes, {
+        "status code should be 200": servicesRes => servicesRes.status === 200,
     });
 
-    sleep(1);
+    const services = servicesRes.json();
+    sleep(0.5);
 
-    /* ###################### */
-    /* ### Step 02        ### */
-    /* ###################### */
-    const step02Body = {
-        "Data": {
-            "Permissions": [
-                "ReadAccountsBasic",
-                "ReadBalances"
-            ],
-            "ExpirationDateTime": "2017-05-02T00:00:00+00:00",
-            "TransactionFromDateTime": "2017-05-03T00:00:00+00:00",
-            "TransactionToDateTime": "2017-12-03T00:00:00+00:00"
-        },
-        "Risk": {}
-    };
+    services.forEach(service => {
+        const serviceViewRes = http.get(`${BASE_URL}/api/services` + service.id + '?messages=true');
+        sleep(0.5);
+        const serviceTestsRes = http.get(`${BASE_URL}/api/tests/service/` + service.id + '?page=0&size=20');
+        sleep(0.5);
+    });
+}
 
-    const step02Headers = {
+/* Simulate applications issuing REST mock invocations . */
+export function invokeRESTMocks() {
+    let pastryCall = http.get(`${BASE_URL}/rest/API+Pastry+-+2.0/2.0.0/pastry`);
+    check(pastryCall, {
+        'pastryCall status is 200': (r) => r.status === 200,
+    });
+
+
+    let eclairCall = http.get(`${BASE_URL}/rest/API+Pastry+-+2.0/2.0.0/pastry/Eclair%20Cafe`);
+    check(eclairCall, {
+        'eclairCall status is 200': (r) => r.status === 200,
+    });
+
+    let eclairXmlCall = http.get(`${BASE_URL}/rest/API+Pastry+-+2.0/2.0.0/pastry/Eclair%20Cafe`, { headers: {'Accept': 'text/xml'} });
+    check(eclairXmlCall, {
+        'eclairXmlCall status is 200': (r) => r.status === 200,
+        'eclairXmlCall response is Xml': (r) => r.body.includes("<pastry>")
+    });
+
+    let millefeuilleCall = http.get(`${BASE_URL}/rest/API+Pastry+-+2.0/2.0.0/pastry/Millefeuille`);
+    check(millefeuilleCall, {
+        'millefeuilleCall status is 200': (r) => r.status === 200,
+    });
+}
+
+/* Simulate applications issuing GraphQL mock invocations . */
+export function invokeGraphQLMocks() {
+    const jsonHeaders = {
         'Content-Type': 'application/json',
     };
-    let step02Response = http.post(`${BASE_URL}/rest/Openbanking.co.uk+Account+and+Transaction+API+v3.1.0/3.1.0/open-banking/v3.1/aisp/account-access-consents`, step02Body, { headers: step02Headers });
 
-    check(step02Response, {
-        'Step 02 Status is 201': (r) => r.status === 201,
+    const allFilmsQuery = "query allFilms {\n" +
+                                " allFilms {\n" +
+                                "   films {\n" +
+                                "      id\n" +
+                                "      title\n" +
+                                "    }\n" +
+                                "  }\n" +
+                                "}";
+    const allFilmsBody = {
+      "query": allFilmsQuery
+    };
+    let allFilmsCall = http.post(`${BASE_URL}/graphql/Movie+Graph+API/1.0`, JSON.stringify(allFilmsBody), { headers: jsonHeaders });
+    check(allFilmsCall, {
+        'allFilmsCall status is 200': (r) => r.status === 200,
     });
 
+    const aFilmQuery = "query film($id: String) {\n" +
+                        " film(id: \"ZmlsbXM6MQ==\") {\n" +
+                        "   id\n" +
+                        "   title\n" +
+                        "   episodeId\n" +
+                        " }\n" +
+                        "}";
+    const aFilmBody = {
+      "query": aFilmQuery
+    };
+    let aFilmCall = http.post(`${BASE_URL}/graphql/Movie+Graph+API/1.0`, JSON.stringify(aFilmBody), { headers: jsonHeaders });
+    check(aFilmCall, {
+        'aFilmCall status is 200': (r) => r.status === 200,
+    });
+
+
+    const aFilmFragmentsQuery = "query film($id: String) {\n" +
+                                  " film(id: \"ZmlsbXM6MQ==\") {\n" +
+                                  "   ...filmFields\n" +
+                                  " }\n" +
+                                  " }\n" +
+                                  " fragment filmFields on Film {\n" +
+                                  "   id\n" +
+                                  "   title\n" +
+                                  "   episodeId\n" +
+                                  "   starCount\n" +
+                                  " }";
+    const aFilmFragmentBody = {
+      "query": aFilmFragmentsQuery
+    };
+    let aFilmFragmentCall = http.post(`${BASE_URL}/graphql/Movie+Graph+API/1.0`, JSON.stringify(aFilmFragmentBody), { headers: jsonHeaders });
+    check(aFilmFragmentCall, {
+        'aFilmFragmentCall status is 200': (r) => r.status === 200,
+    });
+}
+
+/* Simulate applications issuing SOAP mock invocations . */
+export function invokeSOAPMocks() {
+    const andrewBody = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:hel="http://www.example.com/hello">
+                         <soapenv:Header/>
+                         <soapenv:Body>
+                            <hel:sayHello>
+                               <name>Andrew</name>
+                            </hel:sayHello>
+                         </soapenv:Body>
+                      </soapenv:Envelope>`;
+
+    // Check SOAP 1.2 headers.
+    const andrewHeaders = {
+        'Content-Type': 'text/xml',
+        'SOAPAction': 'sayHello'
+    }
+    let andrewCall = http.post(`${BASE_URL}/soap/HelloService+Mock/0.9`, andrewBody, { headers: andrewHeaders });
+    check(andrewCall, {
+        'andrewCall status is 200': (r) => r.status === 200,
+    });
     sleep(1);
 
-    /* ###################### */
-    /* ### Step 03        ### */
-    /* ###################### */
-    let step03 = http.get(`${BASE_URL}/rest/Petstore+API/1.0/v2/pet/findByStatus?status=available&user_key=70f735676ec46351c6699c4bb767878a`);
-    check(step03, {
-        'Step 03 Status is 200': (r) => r.status === 200,
-    });
+    const karlaBody = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:hel="http://www.example.com/hello">
+                             <soapenv:Header/>
+                             <soapenv:Body>
+                                <hel:sayHello>
+                          	 	      <name>Karla</name>
+                                </hel:sayHello>
+                             </soapenv:Body>
+                          </soapenv:Envelope>`;
 
+    // Check SOAP 1.2 headers.
+    const karlaHeaders = {
+        'Content-Type': 'application/soap+xml;action=sayHello'
+    }
+    let karlaCall = http.post(`${BASE_URL}/soap/HelloService+Mock/0.9`, karlaBody, { headers: karlaHeaders });
+    check(karlaCall, {
+        'karlaCall status is 200': (r) => r.status === 200,
+        'karlaCall body is correct': (r) => r.body.includes("<sayHello>Hello Karla !</sayHello>")
+    });
+    sleep(1);
+
+    const laurentHeaders = {
+          'Content-Type': 'application/soap+xml;action=sayHello'
+    }
+    const laurentBody = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:hel="http://www.example.com/hello">
+                         <soapenv:Header/>
+                         <soapenv:Body>
+                            <hel:sayHello>
+                               <name>Laurent</name>
+                            </hel:sayHello>
+                         </soapenv:Body>
+                      </soapenv:Envelope>`;
+
+    let laurentCall = http.post(`${BASE_URL}/soap/HelloService+Mock/0.9`, laurentBody, { headers: laurentHeaders })
+    check(laurentCall, {
+        'laurentCall status is 500': (r) => r.status === 500,
+        'laurentCall body is fault': (r) => r.body.includes("<soapenv:Fault>")
+    });
+    sleep(1);
 }
