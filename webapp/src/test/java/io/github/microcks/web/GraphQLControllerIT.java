65ab635ec4cf20b1c912f47c3774ebbe281d3b94
Laurent Broudoux <laurent.broudoux@gmail.com>
2024-04-24 11:24:10 +0200

    chore: #1134 Remove unused import Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- webapp/src/test/java/io/github/microcks/web/GraphQLControllerIT.java
+++ webapp/src/test/java/io/github/microcks/web/GraphQLControllerIT.java
@@ -25,7 +25,6 @@ import org.springframework.http.ResponseEntity;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
