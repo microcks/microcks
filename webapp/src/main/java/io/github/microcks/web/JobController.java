53035b58b3601a7081e108379f67fe87bafe0248
Laurent Broudoux <laurent.broudoux@gmail.com>
2024-08-27 23:53:15 +0200

    chore: #1290 Remove some code smells Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- webapp/src/main/java/io/github/microcks/web/JobController.java
+++ webapp/src/main/java/io/github/microcks/web/JobController.java
@@ -24,15 +24,17 @@ import io.github.microcks.service.JobService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,19 +52,26 @@ import java.util.Map;
 public class JobController {
 
    /** A simple logger for diagnostic messages. */
-   private static Logger log = LoggerFactory.getLogger(JobController.class);
+   private static final Logger log = LoggerFactory.getLogger(JobController.class);
 
-   @Autowired
-   private ImportJobRepository jobRepository;
+   private final ImportJobRepository jobRepository;
+   private final JobService jobService;
+   private final AuthorizationChecker authorizationChecker;
 
-   @Autowired
-   private JobService jobService;
+   /**
+    * Build a new JobController with its dependencies.
+    * @param jobRepository        to have access to ImportJob definition
+    * @param jobService           to have access to ImportJob service (activate, start, stop)
+    * @param authorizationChecker to check authorization for operations
+    */
+   public JobController(ImportJobRepository jobRepository, JobService jobService,
+         AuthorizationChecker authorizationChecker) {
+      this.jobRepository = jobRepository;
+      this.jobService = jobService;
+      this.authorizationChecker = authorizationChecker;
+   }
 
-   @Autowired
-   private AuthorizationChecker authorizationChecker;
-
-
-   @RequestMapping(value = "/jobs", method = RequestMethod.GET)
+   @GetMapping(value = "/jobs")
    public List<ImportJob> listJobs(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
          @RequestParam(value = "size", required = false, defaultValue = "20") int size,
          @RequestParam(value = "name", required = false) String name) {
@@ -73,25 +82,26 @@ public class JobController {
       return jobRepository.findAll(PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "name"))).getContent();
    }
 
-   @RequestMapping(value = "/jobs/search", method = RequestMethod.GET)
+   @GetMapping(value = "/jobs/search")
    public List<ImportJob> searchJobs(@RequestParam Map<String, String> queryMap) {
       // Parse params from queryMap.
       String name = null;
       Map<String, String> labels = new HashMap<>();
-      for (String paramKey : queryMap.keySet()) {
+      for (Map.Entry<String, String> paramEntry : queryMap.entrySet()) {
+         String paramKey = paramEntry.getKey();
          if ("name".equals(paramKey)) {
-            name = queryMap.get("name");
+            name = paramEntry.getValue();
          }
          if (paramKey.startsWith("labels.")) {
-            labels.put(paramKey.substring(paramKey.indexOf('.') + 1), queryMap.get(paramKey));
+            labels.put(paramKey.substring(paramKey.indexOf('.') + 1), paramEntry.getValue());
          }
       }
 
-      if (labels == null || labels.isEmpty()) {
+      if (labels.isEmpty()) {
          log.debug("Searching jobs corresponding to name {}", name);
          return jobRepository.findByNameLike(name);
       }
-      if (name == null || name.trim().length() == 0) {
+      if (name == null || name.trim().isEmpty()) {
          log.debug("Searching jobs corresponding to labels {}", labels);
          return jobRepository.findByLabels(labels);
       }
@@ -99,7 +109,7 @@ public class JobController {
       return jobRepository.findByLabelsAndNameLike(labels, name);
    }
 
-   @RequestMapping(value = "/jobs/count", method = RequestMethod.GET)
+   @GetMapping(value = "/jobs/count")
    public Map<String, Long> countJobs() {
       log.debug("Counting jobs...");
       Map<String, Long> counter = new HashMap<>();
@@ -107,7 +117,7 @@ public class JobController {
       return counter;
    }
 
-   @RequestMapping(value = "/jobs", method = RequestMethod.POST)
+   @PostMapping(value = "/jobs")
    public ResponseEntity<ImportJob> createJob(@RequestBody ImportJob job) {
       log.debug("Creating new job: {}", job);
       // Store labels somewhere before reinitializing metadata to ensure createdOn is correct.
@@ -124,13 +134,13 @@ public class JobController {
       return new ResponseEntity<>(jobRepository.save(job), HttpStatus.CREATED);
    }
 
-   @RequestMapping(value = "/jobs/{id}", method = RequestMethod.GET)
+   @GetMapping(value = "/jobs/{id}")
    public ResponseEntity<ImportJob> getJob(@PathVariable("id") String jobId) {
       log.debug("Retrieving job with id {}", jobId);
       return new ResponseEntity<>(jobRepository.findById(jobId).orElse(null), HttpStatus.OK);
    }
 
-   @RequestMapping(value = "/jobs/{id}", method = RequestMethod.POST)
+   @PostMapping(value = "/jobs/{id}")
    public ResponseEntity<ImportJob> saveJob(@RequestBody ImportJob job, UserInfo userInfo) {
       log.debug("Saving existing job: {}", job);
       if (authorizationChecker.hasRole(userInfo, AuthorizationChecker.ROLE_ADMIN)
@@ -142,7 +152,7 @@ public class JobController {
       return new ResponseEntity<>(HttpStatus.FORBIDDEN);
    }
 
-   @RequestMapping(value = "/jobs/{id}/activate", method = RequestMethod.PUT)
+   @PutMapping(value = "/jobs/{id}/activate")
    public ResponseEntity<ImportJob> activateJob(@PathVariable("id") String jobId, UserInfo userInfo) {
       log.debug("Activating job with id {}", jobId);
       ImportJob job = jobRepository.findById(jobId).orElse(null);
@@ -156,7 +166,7 @@ public class JobController {
       return new ResponseEntity<>(HttpStatus.FORBIDDEN);
    }
 
-   @RequestMapping(value = "/jobs/{id}/start", method = RequestMethod.PUT)
+   @PutMapping(value = "/jobs/{id}/start")
    public ResponseEntity<ImportJob> startJob(@PathVariable("id") String jobId, UserInfo userInfo) {
       log.debug("Starting job with id {}", jobId);
       ImportJob job = jobRepository.findById(jobId).orElse(null);
@@ -171,7 +181,7 @@ public class JobController {
       return new ResponseEntity<>(HttpStatus.FORBIDDEN);
    }
 
-   @RequestMapping(value = "/jobs/{id}/stop", method = RequestMethod.PUT)
+   @PutMapping(value = "/jobs/{id}/stop")
    public ResponseEntity<ImportJob> stopJob(@PathVariable("id") String jobId, UserInfo userInfo) {
       log.debug("Stopping job with id {}", jobId);
       ImportJob job = jobRepository.findById(jobId).orElse(null);
@@ -185,7 +195,7 @@ public class JobController {
       return new ResponseEntity<>(HttpStatus.FORBIDDEN);
    }
 
-   @RequestMapping(value = "/jobs/{id}", method = RequestMethod.DELETE)
+   @DeleteMapping(value = "/jobs/{id}")
    public ResponseEntity<String> deleteJob(@PathVariable("id") String jobId, UserInfo userInfo) {
       log.debug("Removing job with id {}", jobId);
       ImportJob job = jobRepository.findById(jobId).orElse(null);
