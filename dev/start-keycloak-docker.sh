677043b88a6afb53cc84f87c09bb32a421215d93
Laurent Broudoux <laurent.broudoux@gmail.com>
2024-10-22 16:01:16 +0200

    chore: #1374 Upgrade to Keycloak 26.0.0 Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- dev/start-keycloak-docker.sh
+++ dev/start-keycloak-docker.sh
@@ -1,4 +1,8 @@
+#docker run -it -v $(pwd)/../install/docker-compose/keycloak-realm:/opt/keycloak/data/import -p 8180:8080 \
+#  -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin \
+#  -e KC_HOSTNAME_URL=http://localhost:8180 -e KC_HOSTNAME_ADMIN_URL=http://localhost:8180 \
+#  quay.io/keycloak/keycloak:24.0.4 start-dev --import-realm
+
 docker run -it -v $(pwd)/../install/docker-compose/keycloak-realm:/opt/keycloak/data/import -p 8180:8080 \
   -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin \
-  -e KC_HOSTNAME_URL=http://localhost:8180 -e KC_HOSTNAME_ADMIN_URL=http://localhost:8180 \
-  quay.io/keycloak/keycloak:24.0.4 start-dev --import-realm
+  quay.io/keycloak/keycloak:26.0.0 start-dev --hostname http://localhost:8180 --import-realm
