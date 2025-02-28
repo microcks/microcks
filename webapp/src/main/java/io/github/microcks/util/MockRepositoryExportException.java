208ec14b10859b4ae60a52698ad082690e5006c4
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-02-28 17:01:42 +0100

    feat: #1513 Allow export of AI Copilot samples as APIExamples Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

---
+++ webapp/src/main/java/io/github/microcks/util/MockRepositoryExportException.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.microcks.util;
+
+/**
+ * Typed exception for reporting problem during a mock repository export.
+ * @author laurent
+ */
+public class MockRepositoryExportException extends Exception {
+
+   public MockRepositoryExportException(String message) {
+      super(message);
+   }
+
+   public MockRepositoryExportException(String message, Throwable cause) {
+      super(message, cause);
+   }
+}
