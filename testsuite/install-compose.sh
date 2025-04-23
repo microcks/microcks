b4381afd3951a05ee4e3c89b75ecb4d97d2de1de
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-04-23 14:44:41 +0200

    chore: #1480 change workflow name and triggers, fix indent Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- testsuite/install-compose.sh
+++ testsuite/install-compose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [--method {docker|podman}] [--mode {default|proxy|devmode}] [--addons addon1,addon2,...]"
-    exit 1
+  echo "Usage: $0 [--method {docker|podman}] [--mode {default|proxy|devmode}] [--addons addon1,addon2,...]"
+  exit 1
 }
 
 MODE="default"
@@ -11,30 +11,30 @@ ADDONS=()
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --mode)
-            MODE="$2"
-            shift
-            ;;
-        --addons)
-            IFS=',' read -r -a ADDONS <<< "$2"
-            shift
-            ;;
-        --method)
-            METHOD="$2"
-            shift
-            ;;
-        *)
-            usage
-            ;;
-    esac
-    shift
+  case $1 in
+    --mode)
+      MODE="$2"
+      shift
+      ;;
+    --addons)
+      IFS=',' read -r -a ADDONS <<< "$2"
+      shift
+      ;;
+    --method)
+      METHOD="$2"
+      shift
+      ;;
+    *)
+      usage
+      ;;
+  esac
+  shift
 done
 
 # Validate method flag
 if [[ "$METHOD" != "docker" && "$METHOD" != "podman" ]]; then
-    echo "Error: Invalid method. Choose 'docker' or 'podman'."
-    usage
+  echo "Error: Invalid method. Choose 'docker' or 'podman'."
+  usage
 fi
 
 # Append -compose to form the command name (e.g. docker-compose or podman-compose)
@@ -46,39 +46,39 @@ cd "$SCRIPT_DIR/../install/${METHOD}" || { echo "Failed to change directory to $
 
 # Validate that proxy mode is not allowed when using podman
 if [[ "$METHOD" == "podman-compose" && "$MODE" == "proxy" ]]; then
-    echo "Error: Proxy mode is not supported when using podman."
-    exit 1
+  echo "Error: Proxy mode is not supported when using podman."
+  exit 1
 fi
 
 # Determine main compose file based on the mode
 case $MODE in
-    default)
-        MAIN_COMPOSE="${METHOD}.yml"
-        ;;
-    proxy)
-        MAIN_COMPOSE="${METHOD}-with-proxy.yml"
-        ;;
-    devmode)
-        MAIN_COMPOSE="${METHOD}-devmode.yml"
-        ;;
-    *)
-        echo "Error: Invalid mode. Choose from default, proxy, or devmode."
-        usage
-        ;;
+  default)
+    MAIN_COMPOSE="${METHOD}.yml"
+    ;;
+  proxy)
+    MAIN_COMPOSE="${METHOD}-with-proxy.yml"
+    ;;
+  devmode)
+    MAIN_COMPOSE="${METHOD}-devmode.yml"
+    ;;
+  *)
+    echo "Error: Invalid mode. Choose from default, proxy, or devmode."
+    usage
+    ;;
 esac
 
 # Start building the command with the main compose file
 CMD="${METHOD} -f ${MAIN_COMPOSE}"
 
 for addon in "${ADDONS[@]}"; do
-    addon=$(echo "$addon" | xargs)  # trim whitespace
-    ADDON_FILE="${addon}-addon.yml"
-    if [[ -f "$ADDON_FILE" ]]; then
-        CMD+=" -f ${ADDON_FILE}"
-    else
-        echo "Error: ${ADDON_FILE} not found, installation aborted."
-        exit 1
-    fi
+  addon=$(echo "$addon" | xargs)  # trim whitespace
+  ADDON_FILE="${addon}-addon.yml"
+  if [[ -f "$ADDON_FILE" ]]; then
+    CMD+=" -f ${ADDON_FILE}"
+  else
+    echo "Error: ${ADDON_FILE} not found, installation aborted."
+    exit 1
+  fi
 done
 
 CMD+=" up -d"
@@ -86,8 +86,8 @@ CMD+=" up -d"
 echo "Running command: ${CMD}"
 
 if eval ${CMD}; then
-    echo "Microcks installation complete."
+  echo "Microcks installation complete."
 else
-    echo "Error: ${METHOD} encountered an error, installation aborted."
-    exit 1
+  echo "Error: ${METHOD} encountered an error, installation aborted."
+  exit 1
 fi
