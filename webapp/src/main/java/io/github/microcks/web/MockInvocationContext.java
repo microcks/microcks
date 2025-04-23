9ec91da21328b7ced6828ce57b9dea632c273166
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-04-23 13:20:01 +0200

    feat: #1583 Adding missing class files ;-) Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

---
+++ webapp/src/main/java/io/github/microcks/web/MockInvocationContext.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.microcks.web;
+
+import io.github.microcks.domain.Operation;
+import io.github.microcks.domain.Service;
+
+/**
+ * A mock invocation context that carries the service, operation and resource path.
+ * @param service      The invoked service
+ * @param operation    The invoked operation
+ * @param resourcePath The resource path of the operation used
+ */
+public record MockInvocationContext(Service service, Operation operation, String resourcePath) {
+}
