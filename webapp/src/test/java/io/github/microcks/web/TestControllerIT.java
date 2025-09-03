ada14d43a614137cbce66881d8cf682ee1925021
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-09-03 15:48:28 +0200

    chore: #1714 Adding tests for PostmanTestStepsRunner Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- webapp/src/test/java/io/github/microcks/web/TestControllerIT.java
+++ webapp/src/test/java/io/github/microcks/web/TestControllerIT.java
@@ -19,20 +19,27 @@ package io.github.microcks.web;
 import io.github.microcks.domain.RequestResponsePair;
 import io.github.microcks.domain.TestResult;
 import io.github.microcks.domain.TestStepResult;
+import io.github.microcks.service.TestRunnerService;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 /**
  * Test case for the Test controller.
@@ -42,14 +49,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestControllerIT extends AbstractBaseIT {
 
    @Container
-   public static GenericContainer pastryImpl = new GenericContainer("quay.io/microcks/quarkus-api-pastry:latest")
+   public static GenericContainer<?> pastryImpl = new GenericContainer("quay.io/microcks/quarkus-api-pastry:latest")
          .withExposedPorts(8282);
 
    @Container
-   public static GenericContainer helloWorldImpl = new GenericContainer("quay.io/microcks/grpc-hello-world:nightly")
+   public static GenericContainer<?> helloWorldImpl = new GenericContainer("quay.io/microcks/grpc-hello-world:nightly")
          .withExposedPorts(9000);
 
-   @SpyBean
+   @Container
+   public static GenericContainer<?> postmanRunner = new GenericContainer("quay.io/microcks/microcks-postman-runtime:nightly")
+         .waitingFor(Wait.forLogMessage(".*postman-runtime wrapper listening on port.*", 1))
+         .withExposedPorts(3000);
+
+   @Container
+   public static GenericContainer<?> goodPastryImpl = new GenericContainer("quay.io/microcks/contract-testing-demo:03")
+         .withExposedPorts(3003);
+
+   @Autowired
+   private TestRunnerService testRunnerService;
+
+   @MockitoSpyBean
    private TestController testController;
 
    @Test
@@ -77,11 +96,7 @@ class TestControllerIT extends AbstractBaseIT {
       assertEquals(testEndpoint, testResult.getTestedEndpoint());
 
       // Wait till timeout and re-fetch the result.
-      try {
-         Thread.sleep(2000);
-      } catch (InterruptedException e) {
-         throw new RuntimeException(e);
-      }
+      waitForTestCompletion(testResult, 3);
 
       response = restTemplate.getForEntity("/api/tests/" + testResult.getId(), TestResult.class);
       assertEquals(200, response.getStatusCode().value());
@@ -98,6 +113,52 @@ class TestControllerIT extends AbstractBaseIT {
       assertEquals(1, pairs.size());
       assertEquals("pastries_json", pairs.get(0).getRequest().getName());
    }
+
+
+   @DynamicPropertySource
+   static void postmanRunnerEndpoint(DynamicPropertyRegistry registry) {
+      registry.add("postman-runner.url", () -> String.format("http://localhost:%d", postmanRunner.getMappedPort(3000)));
+   }
+
+   @Test
+   void testPostmanTesting() {
+      // Upload API Pastry Contract Testing demo reference artifacts.
+      uploadArtifactFile("target/test-classes/io/github/microcks/util/postman/apipastries-openapi.yaml", true);
+      uploadArtifactFile("target/test-classes/io/github/microcks/util/postman/apipastries-postman-collection.json", false);
+
+      String testEndpoint = String.format("http://host.docker.internal:%d", goodPastryImpl.getMappedPort(3003));
+      testRunnerService.setTestsCallbackUrl(getServerUrl().replace("localhost", "host.docker.internal"));
+
+      StringBuilder testRequest = new StringBuilder("{").append("\"serviceId\": \"API Pastries:0.0.1\", ")
+            .append("\"testEndpoint\": \"").append(testEndpoint).append("\", ").append("\"runnerType\": \"POSTMAN\", ")
+            .append("\"timeout\": 2000").append("}");
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.APPLICATION_JSON);
+      HttpEntity<String> entity = new HttpEntity<>(testRequest.toString(), headers);
+
+      ResponseEntity<TestResult> response = restTemplate.postForEntity("/api/tests", entity, TestResult.class);
+      assertEquals(201, response.getStatusCode().value());
+
+      TestResult testResult = response.getBody();
+      assertNotNull(testResult);
+      assertNotNull(testResult.getId());
+      assertTrue(testResult.isInProgress());
+      assertEquals(testEndpoint, testResult.getTestedEndpoint());
+
+      // Wait till timeout and re-fetch the result.
+      waitForTestCompletion(testResult, 3);
+
+      response = restTemplate.getForEntity("/api/tests/" + testResult.getId(), TestResult.class);
+      assertEquals(200, response.getStatusCode().value());
+
+      testResult = response.getBody();
+      assertNotNull(testResult);
+      assertFalse(testResult.isInProgress());
+      assertTrue(testResult.isSuccess());
+      assertEquals(3, testResult.getTestCaseResults().size());
+   }
+
 
    @Test
    void testSoapUITesting() {
@@ -124,11 +185,7 @@ class TestControllerIT extends AbstractBaseIT {
       assertEquals(testEndpoint, testResult.getTestedEndpoint());
 
       // Wait till timeout and re-fetch the result.
-      try {
-         Thread.sleep(2000);
-      } catch (InterruptedException e) {
-         throw new RuntimeException(e);
-      }
+      waitForTestCompletion(testResult, 3);
 
       response = restTemplate.getForEntity("/api/tests/" + testResult.getId(), TestResult.class);
       assertEquals(200, response.getStatusCode().value());
@@ -190,11 +247,7 @@ class TestControllerIT extends AbstractBaseIT {
       assertEquals(testEndpoint, testResult.getTestedEndpoint());
 
       // Wait till timeout and re-fetch the result.
-      try {
-         Thread.sleep(2000);
-      } catch (InterruptedException e) {
-         throw new RuntimeException(e);
-      }
+      waitForTestCompletion(testResult, 3);
 
       response = restTemplate.getForEntity("/api/tests/" + testResult.getId(), TestResult.class);
       assertEquals(200, response.getStatusCode().value());
@@ -242,11 +295,7 @@ class TestControllerIT extends AbstractBaseIT {
       assertEquals(testEndpoint, testResult.getTestedEndpoint());
 
       // Wait till timeout and re-fetch the result.
-      try {
-         Thread.sleep(2000);
-      } catch (InterruptedException e) {
-         throw new RuntimeException(e);
-      }
+      waitForTestCompletion(testResult, 3);
 
       response = restTemplate.getForEntity("/api/tests/" + testResult.getId(), TestResult.class);
       assertEquals(200, response.getStatusCode().value());
@@ -256,4 +305,17 @@ class TestControllerIT extends AbstractBaseIT {
       assertFalse(testResult.isSuccess());
    }
 
+   private void waitForTestCompletion(TestResult testResult, int seconds) {
+      await().atMost(seconds, TimeUnit.SECONDS)
+            .pollDelay(500, TimeUnit.MILLISECONDS)
+            .pollInterval(250, TimeUnit.MILLISECONDS)
+            .until(() -> {
+               ResponseEntity<TestResult> resp = restTemplate.getForEntity("/api/tests/" + testResult.getId(), TestResult.class);
+               if (resp.getStatusCode().value() == 200) {
+                  TestResult tr = resp.getBody();
+                  return tr != null && !tr.isInProgress();
+               }
+               return false;
+            });
+   }
 }
