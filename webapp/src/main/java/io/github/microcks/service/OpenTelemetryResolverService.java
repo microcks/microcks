86c055df23ad820c8457d047731b1d65ce78187c
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-10-06 15:29:20 +0200

    chore: #1728 Adding missing file headers Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- webapp/src/main/java/io/github/microcks/service/OpenTelemetryResolverService.java
+++ webapp/src/main/java/io/github/microcks/service/OpenTelemetryResolverService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.microcks.service;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
@@ -14,10 +29,18 @@ import org.springframework.stereotype.Service;
 public class OpenTelemetryResolverService {
    private final ApplicationContext applicationContext;
 
+   /**
+    * Build a resolver service with the given application context.
+    * @param applicationContext The Spring application context
+    */
    public OpenTelemetryResolverService(ApplicationContext applicationContext) {
       this.applicationContext = applicationContext;
    }
 
+   /**
+    * Get an OpenTelemetry instance from the application context or fall back to the global instance.
+    * @return An OpenTelemetry instance
+    */
    public OpenTelemetry getOpenTelemetry() {
       OpenTelemetry openTelemetry;
       try {
